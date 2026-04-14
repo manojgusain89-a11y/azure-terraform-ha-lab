85b84e010006af5344f44237b7f1b5b4f8dbd947
Singh <manoj.singh@knipper.com>
2026-04-14 17:04:03 +0530

    storage

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = " rg-backend_lab "
+    resource_group_name  = "rg-backend_lab"
     storage_account_name = "tfbackend4857745"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
